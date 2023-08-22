89e97073951b827b09f8ef695edc83699ad1c515
BrianFernandoJara <brianfernandojara@gmail.com>
2023-08-22 18:16:41 -0400

    Functions lab continued complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity){
+    if( activity !== undefined ){
+        return `This Saturday, I want to ${activity}!`;
+    }else{
+        return `This Saturday, I want to roller-skate!`;
+    }
+}
+const mondayWork = function(activity){
+    if(activity !== undefined){
+        return `This Monday, I will ${activity}.`;
+    }else{
+        return `This Monday, I will go to the office.`;
+    }
+}
+function wrapAdjective(emphasize = "*"){
+    return function(adjective = "special"){ 
+        return `You are ${emphasize+adjective+emphasize}!`;
+    }
+}
